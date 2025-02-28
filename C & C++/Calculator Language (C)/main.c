f130d029fa5e9cf6bef9a897ab6ff4b4bda8007a
Plunyo <conradthieu87@gmail.com>
2025-02-28 11:22:14 +1100

    yuh

--- C & C++/Calculator Language (C)/main.c
+++ C & C++/Calculator Language (C)/main.c
@@ -1,40 +1,41 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
 #define MAX_NUM_LENGTH 64
 
-typedef enum {
-    NUMBER,
-    MULTIPLY,
-    ADD,
-    DIVIDE,
-    SUBTRACT,
-    MODULO
-} TokenType;
+typedef enum { NUMBER, MULTIPLY, ADD, DIVIDE, SUBTRACT, MODULO } TokenType;
 
 typedef struct {
-    TokenType type;
-    double value;
+        TokenType type;
+        double value;
 } Token;
+
+typedef struct {
+        int token_count;
+        Token *tokens;
+} TokenizerInfo;
 
 void print_token(Token *token) {
     printf("{ Value: %f | Type: %d }", token->value, token->type);
 }
 
-Token* tokenize(char* source_code) {
+TokenizerInfo tokenize(char *source_code) {
     int token_count = strlen(source_code); // every char is a token
-    Token* tokens = malloc(token_count * sizeof(Token)); // malloc to token_count * sizeof(Token)
-    
-    if (tokens == NULL) printf("token array malloc failed ;(");
+    Token *tokens = malloc(
+        token_count * sizeof(Token)); // malloc to token_count * sizeof(Token)
+
+    if (tokens == NULL)
+        printf("token array malloc failed ;(");
 
     for (int i = 0; i < token_count; i++) {
-        if (isdigit(source_code[i])) { // build number tokens
+        if (isdigit(source_code[i])) {    // build number tokens
             char num_str[MAX_NUM_LENGTH]; // 64 buffer
             int num_index = i;
 
-            while (isdigit(source_code[num_index]) && num_index > MAX_NUM_LENGTH) { // while its still a number
+            while (isdigit(source_code[num_index]) &&
+                   num_index > MAX_NUM_LENGTH) { // while its still a number
                 num_str[num_index] = source_code[num_index];
                 num_index++;
             }
@@ -43,31 +44,31 @@ Token* tokenize(char* source_code) {
             i += num_index;
         }
     }
-    
-    return tokens;
+
+    return (TokenizerInfo){token_count, tokens};
 }
 
 int main(int argc, char *argv[]) {
     // basic repl
     while (1) {
         char input[128];
-        
+
         printf("> ");
         fgets(input, sizeof(input), stdin);
         input[strcspn(input, "\n")] = '\0';
-        if (strcmp("exit", input) == 0) break;
+        if (strcmp("exit", input) == 0)
+            break;
 
-        Token* tokens = tokenize(input);
+        TokenizerInfo tokenizer_info = tokenize(input);
 
-        if (tokens == NULL) {
+        if (tokenizer_info.tokens == NULL) {
             printf("token malloc failed ;(");
             continue;
         }
 
-        for (int i = 0; i < sizeof(&tokens) / sizeof(tokens[0]); i++) {
-        } 
-    
+        for (int i = 0; i < tokenizer_info.token_count; i++) {
+        }
     }
-    
+
     return EXIT_SUCCESS;
 }
